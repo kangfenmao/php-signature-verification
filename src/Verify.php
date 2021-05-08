bdbb66d1bb6eb24ecb11082c9a49c60ead3b1098
亢奋猫 <kangfenmao@qq.com>
2021-05-08 10:30:37 +0800

    fix(path): binary path error

--- src/Verify.php
+++ src/Verify.php
@@ -4,9 +4,10 @@ namespace Kangfenmao\SignatureVerification;
 
 class Verify
 {
-  public static function verify(string $value, string $signature): bool
-  {
-      exec("../bin/verify $value $signature 2>&1", $output, $status);
-      return !$status;
-  }
+    public static function verify(string $value, string $signature): bool
+    {
+        $command = dirname(__FILE__).'/../bin/verify';
+        exec("$command $value $signature 2>&1", $output, $status);
+        return !$status;
+    }
 }
