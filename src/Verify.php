a19bba1e67fdeaacb4bc9927c87e4f9344e4f967
亢奋猫 <kangfenmao@qq.com>
2021-05-08 15:09:35 +0800

    fix(os): fix linux binary can not execute

--- src/Verify.php
+++ src/Verify.php
@@ -6,7 +6,12 @@ class Verify
 {
     public static function verify(string $value, string $signature): bool
     {
-        $command = dirname(__FILE__).'/../bin/verify';
+        $command = dirname(__FILE__).'/../bin/verify_'.PHP_OS;
+
+        if (!file_exists($command)) {
+            return false;
+        }
+
         exec("$command $value $signature 2>&1", $output, $status);
         return !$status;
     }
